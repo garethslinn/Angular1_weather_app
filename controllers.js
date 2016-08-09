4d106689df3e2e6f014358fb4c99b2b554ca6ec6
Gareth Slinn <gslinn@gmail.com>
2016-08-09 11:46:26 +0100

    first commit

---
+++ controllers.js
@@ -0,0 +1,36 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?q=London,uk&units=metric&appid=38f05f0826b2c6dc123c36314a9ecd6f", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+    $scope.roundUpTemp = function(degC) {
+
+        return Math.ceil(degC)
+
+    }
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
+
+
+    
+}]);
